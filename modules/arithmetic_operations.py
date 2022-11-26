891a6d0a144d09c171312fc8604a1d1606b44c3a
Taisiia <kiprictaisia2@gmail.com>
2022-11-26 15:07:58 +0100

    26,11

---
+++ modules/arithmetic_operations.py
@@ -0,0 +1,35 @@
+from modules.lists import list_input
+from modules.labels import label
+
+# def arithmetic_operation():
+#     if list_input[1] == "+":
+#         number = int(list_input[0]) + int(list_input[2])
+#     elif list_input[1] == "-":
+#         number = int(list_input[0]) - int(list_input[2])
+#     elif list_input[1] == "*":
+#         number = int(list_input[0]) * int(list_input[2])
+#     elif list_input[1] == "/":
+#         try:
+#             number = int(list_input[0]) / int(list_input[2])   
+#         except:
+#             number = "Can`t divide by zero"
+#     elif list_input[1] == "%":
+#         number = (int(list_input[0]) * int(list_input[2]))/100
+#     return number 
+
+def arithmetic_operation():
+    if list_input[1] == "+":
+        number = float(list_input[0]) + float(list_input[2])
+    elif list_input[1] == "-":
+        number = float(list_input[0]) - float(list_input[2])
+    elif list_input[1] == "*":
+        number = float(list_input[0]) * float(list_input[2])
+    elif list_input[1] == "%":
+        number = float(list_input[0]) * float(list_input[2])
+        number = number / 100
+    elif list_input[1] == "/":
+        try:
+            number = float(list_input[0]) / float(list_input[2])   
+        except:
+            number = "Can`t divide by zero"
+    return number 
